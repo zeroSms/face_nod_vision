6fcc1a853055506a9addf7c289f914e120e80753
zeroSms <s185076z@st.go.tuat.ac.jp>
2021-11-15 19:59:27 +0900

    空の入力でエラー処理

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quiver/iterables.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +19,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+        ),
+        body: const MyHomePage(),
+      ),
     );
   }
 }
@@ -28,11 +32,7 @@ class MyApp extends StatelessWidget {
 // StatefulWidget: 状態が変化するWidget
 class MyHomePage extends StatefulWidget {
   // @required 必須なパラメータに付与．不指定の場合に警告を出してくれるため，引数の渡し忘れに気づける．
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // このウィジェットは，アプリケーションのホーム・ページです．ステートフルとは、見た目に影響を与えるフィールドを含むステート・オブジェクト（以下に定義）を持つことを意味します。このクラスは、ステートの設定です。このクラスは、親(ここではAppウィジェット)から提供され、Stateのbuildメソッドで使用される値(ここではタイトル)を保持します。Widgetサブクラスのフィールドは、常に "final "と表示されます。
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   // StatefulWidget(imutable) 自体は build() メソッドを持たず、代わりに createState() で生成した State オブジェクトが子孫となる Widget を生成。その State オブジェクトは可変（mutable) で任意のデータを保持できるため、ユーザー操作などに応じてデータを変化させつつ State.setState で能動的にリビルドを発生させられるのが StatefulWidget
   @override
@@ -40,15 +40,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   // setState（状態を保持するメソッド）によって再度画面が描画
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style =
@@ -59,30 +51,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Form(
+      key: _formKey,
+      child: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
@@ -96,17 +68,17 @@ class _MyHomePageState extends State<MyHomePage> {
             const SizedBox(height: 30),
             ElevatedButton(
               style: style,
-              onPressed: () {},
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')));
+                }
+              },
               child: const Text('Connection'),
             ),
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
